1ff2833f5cb250d22810e02913f9f048521d219b
Yingwei Zheng <dtcxzyw2333@gmail.com>
2025-03-29 01:51:19 +0800

    refine corpus

--- collect.py
+++ collect.py
@@ -50,7 +50,9 @@ for line in diagnostic_strings:
     strings.append(ast.literal_eval(line))
 
 
-def get_custom_messages(path, keyword, suffix=False, trunc_for_suffix=False):
+def get_custom_messages(
+    path, keyword, suffix=False, trunc_for_suffix=False, max_len=10000000
+):
     count = 0
     global strings
 
@@ -85,6 +87,8 @@ def get_custom_messages(path, keyword, suffix=False, trunc_for_suffix=False):
                         depth -= 1
                         if depth == 0:
                             break
+                    if pos == len(srcstr) - 1:
+                        break
                     pos += 1
                 expr = srcstr[beg:pos]
                 if srcstr[beg - 1 : beg + 2] != 'R"(':
@@ -122,11 +126,15 @@ def get_custom_messages(path, keyword, suffix=False, trunc_for_suffix=False):
                     continue
                 while substr.startswith("(") and substr.endswith(")"):
                     substr = substr[1:-1]
-                if len(substr) != 0 and not (
-                    ("-" in substr) and (" " not in substr) and ("<" not in substr)
+                if (
+                    len(substr) != 0
+                    and not (
+                        ("-" in substr) and (" " not in substr) and ("<" not in substr)
+                    )
+                    and len(substr) < max_len
                 ):
                     strings.append(substr)
-                count += 1
+                    count += 1
     return count
 
 
@@ -178,7 +186,12 @@ for line in option_strings:
 inline_options_count = 0
 inline_options_count += get_custom_messages(".", "cl::desc(")
 inline_options_count += get_custom_messages(".", "clEnumValN(", suffix=True)
+inline_options_count += get_custom_messages(".", "clEnumVal(", suffix=True)
 inline_options_count += get_custom_messages(".", "cl::OptionCategory")
+inline_options_count += get_custom_messages(".", "cl::OptionCategory", suffix=True)
+inline_options_count += get_custom_messages(
+    "llvm/include/llvm/Target", "addLiteralOption(", suffix=True
+)
 print("Inline Option:", inline_options_count)
 
 # Passes description using legacy pass manager
@@ -187,11 +200,32 @@ old_passes_count += get_custom_messages(
     ".", "INITIALIZE_PASS_BEGIN(", suffix=True, trunc_for_suffix=True
 )
 old_passes_count += get_custom_messages(
+    ".", "INITIALIZE_PASS_END(", suffix=True, trunc_for_suffix=True
+)
+old_passes_count += get_custom_messages(
     ".", "INITIALIZE_PASS(", suffix=True, trunc_for_suffix=True
 )
+old_passes_count += get_custom_messages(
+    ".", "static RegisterPass<", suffix=True, trunc_for_suffix=True
+)
+old_passes_count += get_custom_messages("llvm/lib", "_NAME ", max_len=100)
 print("Old Passes:", old_passes_count)
 desc_count = get_custom_messages(".", "cl::ParseCommandLineOptions(", suffix=True)
 print("Program Desc:", desc_count)
+sched_count = 0
+sched_count += get_custom_messages(
+    "llvm/lib", "static MachineSchedRegistry", suffix=True, trunc_for_suffix=True
+)
+sched_count += get_custom_messages(
+    "llvm/lib", "static RegisterScheduler", suffix=True, trunc_for_suffix=True
+)
+print("Schedulers:", sched_count)
+debug_count = get_custom_messages("llvm/lib", "DEBUG_COUNTER(", suffix=True)
+print("Debug Counter:", debug_count)
+reg_alloc_count = get_custom_messages(
+    "llvm/lib/CodeGen", "static RegisterRegAlloc", suffix=True, trunc_for_suffix=True
+)
+print("Register Allocators:", reg_alloc_count)
 
 # Special strings
 strings.append("clang LLVM compiler")
@@ -225,8 +259,23 @@ strings.append(
     "PLEASE ATTACH THE FOLLOWING FILES TO THE BUG REPORT:\n"
     "Preprocessed source(s) and associated run script(s) are located at:"
 )
+strings.append("Override the behaviour of expand-variadics")
+strings.append(
+    "Options: <empty>|Legal|Discard|Convert. If non-empty, ignore TargetTransformInfo and always use this transformation for the %evl parameter (Used in testing)."
+)
 
 strings = list(set(filter(lambda x: x.lower() != x.upper(), strings)))
+strings.remove("All")
+strings.remove("all")
+strings.remove("never")
+strings.remove("Enabled")
+strings.remove("Disabled")
+strings.remove("Default")
+strings.remove("directory")
+strings.remove("filename")
+strings.remove(" position!")
+strings.remove(" version ")
+strings.remove("' attribute: ")
 strings.sort()
 
 with open(output_path, "w") as f:
